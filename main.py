2b416641f8a2eec100cbc8d1c6919ecdeae28401
Egor Proskurine <dierotefarbe@gmail.com>
2021-03-20 12:18:09 +0300

    skeleton

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 
+class Thing():
+    __init__():
+        pass
+class Person():
+    __init__():
+        pass
+class Paladin():
+    __init__():
+        pass
+class Warrior():
+    __init__():
+        pass
