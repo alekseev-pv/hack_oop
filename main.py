86d415fccdda1c2c837099ff53f1d1dd34ee7cac
mehonator <mehonator@yandex.ru>
2021-03-20 11:54:26 +0300

    create class Thing

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-
+class Thing:
+    def __init__(
+        self,
+        name: str,
+        protection: float,
+        attack_damage: float,
+        hit_points: float,
+    ):
+        self.name = name
+        self.protection = protection
+        self.attack_damage = attack_damage
+        self.hit_points = hit_points
