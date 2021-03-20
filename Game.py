bbb051d82f49a0ae0c0b873bbb83914959bc5cdc
Evgenii <zhenya.hvan@gmail.com>
2021-03-20 13:55:38 +0300

    first commit

--- Game.py
+++ Game.py
@@ -1,10 +1,17 @@
 """
 Игра арена
+
+Игра, основанная по мотивам 'The Elder Scrolls: Arena' 1994г.
 """
 
 
 class Person():
-    pass
+    def __init__(self, name) -> None:
+        self.name = name
+        self.hp: int = 10
+        self.attack: int = 1
+        self.protect: float = 0.01
+        print('Создан персонаж по имени {0} с кол-ом здоровья {1}, атакой: {2} и защитой {3}%'.format(self.name, self.hp, self.attack, int(self.protect*100)))
 
 
 class Thing():
@@ -17,3 +24,6 @@ class Paladin(Person):
 
 class Warrior(Person):
     pass
+
+
+first_hero = Person("Hero")
