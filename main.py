a2b93830a529bcd3f24851ce644b35dabde8e396
Anton <logas83@gmail.com>
2021-03-20 19:06:35 +0500

    create classes and methods2

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import random
+
+names = ["Тор", "Конан", "Фесс", "Гром", "Зевс", 
+        "Марс", "Один", "Геракл", "Зэна", "Диана", 
+        "Афина", "Титан", "Спартак", "Гаргона", "Арагорн", 
+        "Боромир", "Леонид", "Гор", "Онурис", "Нейт"]
+
+# Выбор случайного имени из списка имён
+def rnd_names():
+    name = random.choice(names)
+    return name
+
 # Класс содержит в себе следующие параметры - название, процент защиты, атаку и жизнь
 # Это могут быть предметы одежды, магические кольца, всё что угодно)
+
+
 class Thing:
     def __init__(self, thing_name, thing_defense, thing_attack, thing_life):
         self.thing_name = thing_name
@@ -19,11 +33,11 @@ class Person:
         self.pers_defense = pers_defense
 
     # метод, принимающий на вход список вещей
-    def setThings(things):
+    def set_things(self, things):
         pass
 
     # метод вычитания жизни на основе входной атаки
-    def life_minus(attack_in):
+    def life_minus(self, attack_in):
         pass
 
 # Класс наследуется от персонажа, при этом количество присвоенных жизней и процент защиты умножается на 2 в конструкторе
