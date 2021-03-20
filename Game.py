9197f3b2a0dad8d814276a9f632d979c04619bfe
Евгений <zhenya.hvan@gmail.com>
2021-03-20 21:36:16 +0300

    work_version

--- Game.py
+++ Game.py
@@ -1,5 +1,5 @@
 """
-Игра арена
+Игра "Арена"
 (Основана по мотивам 'The Elder Scrolls: Arena' 1994г.)
 
 На арене будут сражаться все известные вам персонажи, от простых купцов
@@ -7,6 +7,9 @@
 
 """
 import random as rd
+import colorama
+
+from colorama import Fore, Back, Style
 from random import randint
 from operator import attrgetter
 
@@ -14,9 +17,9 @@ from operator import attrgetter
 class Person:
     def __init__(self, name) -> None:
         self.name = name
-        self.hp: int = 10
-        self.attack: int = 1
-        self.protect: int = 0
+        self.hp: float = 10
+        self.attack: float = 1
+        self.protect: float = 0
         self.things = []
 
     def setThing(self, thing) -> None:
@@ -28,10 +31,6 @@ class Person:
     def get_damaged(self, attack_damage) -> None:
         self.hp = round(self.hp - attack_damage - attack_damage*(self.protect/100), 2)
 
-
-    def statusHero(self) -> str:
-        output: str = 'Имя {}, здоровье {}, атака {}, защита {}%.'.format(self.name, self.hp, self.attack, self.protect)
-        return output
 
 class Thing:
     def __init__(self) -> None:
@@ -46,19 +45,27 @@ class Paladin(Person):
         self.hp = self.hp * 2
         self.protect = self.protect * 2
 
-    def status(self) -> str:
-        return self.statusHero()
 
 class Warrior(Person):
     def __init__(self, name) -> None:
         Person.__init__(self, name)
         self.attack = self.attack * 2
 
-    def status(self) -> str:
-        return self.statusHero()
+
+class Orc(Person):
+    def __init__(self, name) -> None:
+        Person.__init__(self, name)
+        self.hp = self.hp * 1.5
 
 
-def fight(heroes):
+class Thief(Person):
+    def __init__(self, name) -> None:
+        Person.__init__(self, name)
+        self.attack = self.attack * 2.5
+        self.hp = self.hp - 2
+
+
+def fight(heroes) -> str:
     while len(heroes) > 1:
         dual = rd.sample(heroes, k=2)
         attack = dual.pop(randint(0,1))
@@ -66,22 +73,30 @@ def fight(heroes):
         defender.get_damaged(attack.attack)
         print('{} наносит удар по {} на {} урона'.format(attack.name, defender.name, attack.attack))
         if defender.hp < 0:
+            colorama.init()
+            print(Fore.RED + '{} получает смертельное ранение и погибает (но т.к. он даэдра, с ним все хорошо)'.format(defender.name), end="")
+            print(Style.RESET_ALL)
             heroes.remove(defender)
     
     if len(heroes) == 1:
         winner = heroes.pop()
-        return('Побеидил {}! У него осталось {} здоровья'.format(winner.name, winner.hp))
+        return(Fore.YELLOW + 'Победил {}!'.format(winner.name))
     else:
-        return('Все погибли, никто не победил!')
+        return(Fore.RED + 'Все погибли, никто не победил!')
 
+
+def creation_of_heroes(name_of_hero, bonus_things) -> list:
+    heroes = [Paladin(rd.choice(name_of_hero)) for _ in range(5)]
+    heroes += [Warrior(rd.choice(name_of_hero)) for _ in range(5)]
+    heroes += [Orc(rd.choice(name_of_hero)) for _ in range(5)]
+    heroes += [Thief(rd.choice(name_of_hero)) for _ in range(5)]
+    for hero in heroes:
+        for bonus_thing in rd.sample(bonus_things, k=randint(1,4)):
+            hero.setThing(bonus_thing)
+    return heroes
 
 name_of_hero = ["Азура", "Боэтия", "Вермина", "Джиггалаг", "Клавикус", "М'Айк", "Малакат", "Меридия", "Мерунес Дагон", "Мефала", "Молаг Бал", "Намира", "Ноктюрнал", "Периайт", "Сангвин", "Ситис", "Уриэль Септим", "Хермеус Мора", "Хирсин", "Шеогорат"]
-bonus_things = sorted([Thing() for _ in range(20)], key=lambda thing: thing.bonus_protect)
-heroes_paladin = [Paladin(rd.choice(name_of_hero)) for _ in range(10)]
-heroes_warrior = [Warrior(rd.choice(name_of_hero)) for _ in range(10)]
-heroes = heroes_paladin + heroes_warrior
-for hero in heroes:
-    for bonus_thing in rd.sample(bonus_things, k=randint(1,4)):
-        hero.setThing(bonus_thing)
+bonus_things = sorted([Thing() for _ in range(20)], key=lambda thing: thing.bonus_protect) #сортировка вещей по защите
 
-print(fight(heroes))
+print(fight(creation_of_heroes(name_of_hero, bonus_things)))
+print(Style.RESET_ALL)
