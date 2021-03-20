e6ca3b09eda833143c97574ef03112fc6e3de404
Павел Стерехов <iiabejic@gmail.com>
2021-03-20 22:36:08 +0500

    Update

--- game_arena.py
+++ game_arena.py
@@ -2,26 +2,16 @@ import random
 
 
 class Thing:
-    NAMES_THING = ['шлем', 'нагрудник', 'наплечники',
-                   'перчатки', 'пояс', 'поножи',
-                   'сапоги', 'меч', 'топор', 'щит']
-
-    def __init__(self):
-        self.name = random.choice(self.NAMES_THING)
-        self.defense = round(random.uniform(0, 10)/100, 2)
-        self.attack = random.randint(0, 10)
-        self.hp = random.randint(0, 30)
+    def __init__(self, name, defense, attack, hp):
+        self.name = name
+        self.defense = defense
+        self.attack = attack
+        self.hp = hp
 
 
 class Person:
-    NAMES_PERSON = ['Халвадор', 'Линетта', 'Каваи', 'Декрин',
-                    'Аромет', 'Сукмот', 'Рохиеса', 'Форина',
-                    'Миркан', 'Кристол', 'Бутрон', 'Трин',
-                    'Миркана', 'Ворстол', 'Битра', 'Амаури'
-                    'Джелли', 'Фиокир', 'Трикон', 'Янкир']
-
-    def __init__(self):
-        self.name = random.choice(self.NAMES_PERSON)
+    def __init__(self, name):
+        self.name = name
         self.hp = 100
         self.attack = 5
         self.defense = 0.1
@@ -51,4 +41,29 @@ class Warrior(Person):
 
 
 class Arena:
-    pass
+    NAMES_THING = ['шлем', 'нагрудник', 'наплечники',
+                   'перчатки', 'пояс', 'поножи',
+                   'сапоги', 'меч', 'топор', 'щит']
+    NAMES_PERSON = ['Халвадор', 'Линетта', 'Каваи', 'Декрин',
+                    'Аромет', 'Сукмот', 'Рохиеса', 'Форина',
+                    'Миркан', 'Кристол', 'Бутрон', 'Трин',
+                    'Миркана', 'Ворстол', 'Битра', 'Амаури'
+                    'Джелли', 'Фиокир', 'Трикон', 'Янкир']
+
+    def creat_things(self):
+        things = []
+        thing = Thing(
+            name=random.choice(self.NAMES_THING),
+            defense=round(random.uniform(0, 10)/100, 2),
+            attack=random.randint(0, 10),
+            hp=random.randint(0, 30))
+        count = 80
+        if len(things) < count:
+            things.append(thing)
+            count -= count
+        things.sort()
+
+    def creat_characters(self):
+        characters = []
+        count = random.randint(1,2)
+
