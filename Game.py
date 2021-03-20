24cb1aa8410ec493b0552ed13a173b05bdecc841
Евгений <zhenya.hvan@gmail.com>
2021-03-20 21:56:25 +0300

    final_version

--- Game.py
+++ Game.py
@@ -64,28 +64,30 @@ class Thief(Person):
         self.attack = self.attack * 2.5
         self.hp = self.hp - 2
 
+def decorate(fcn):
+    def fight(name_of_hero, bonus_things) -> str:
+        heroes = fcn(name_of_hero, bonus_things)
+        while len(heroes) > 1:
+            dual = rd.sample(heroes, k=2)
+            attack = dual.pop(randint(0,1))
+            defender = dual.pop()
+            defender.get_damaged(attack.attack)
+            print('{} наносит удар по {} на {} урона'.format(attack.name, defender.name, attack.attack))
+            if defender.hp < 0:
+                colorama.init()
+                print(Fore.RED + '{} получает смертельное ранение и погибает (но т.к. он даэдра, с ним все хорошо)'.format(defender.name), end="")
+                print(Style.RESET_ALL)
+                heroes.remove(defender)
+        
+        if len(heroes) == 1:
+            winner = heroes.pop()
+            print(Fore.YELLOW + 'Победил {}!'.format(winner.name))
+        else:
+            print(Fore.RED + 'Все погибли, никто не победил!')
+    return fight
 
-def fight(heroes) -> str:
-    while len(heroes) > 1:
-        dual = rd.sample(heroes, k=2)
-        attack = dual.pop(randint(0,1))
-        defender = dual.pop()
-        defender.get_damaged(attack.attack)
-        print('{} наносит удар по {} на {} урона'.format(attack.name, defender.name, attack.attack))
-        if defender.hp < 0:
-            colorama.init()
-            print(Fore.RED + '{} получает смертельное ранение и погибает (но т.к. он даэдра, с ним все хорошо)'.format(defender.name), end="")
-            print(Style.RESET_ALL)
-            heroes.remove(defender)
-    
-    if len(heroes) == 1:
-        winner = heroes.pop()
-        return(Fore.YELLOW + 'Победил {}!'.format(winner.name))
-    else:
-        return(Fore.RED + 'Все погибли, никто не победил!')
-
-
-def creation_of_heroes(name_of_hero, bonus_things) -> list:
+@decorate
+def creation_of_heroes_and_fight(name_of_hero, bonus_things) -> list:
     heroes = [Paladin(rd.choice(name_of_hero)) for _ in range(5)]
     heroes += [Warrior(rd.choice(name_of_hero)) for _ in range(5)]
     heroes += [Orc(rd.choice(name_of_hero)) for _ in range(5)]
@@ -98,5 +100,5 @@ def creation_of_heroes(name_of_hero, bonus_things) -> list:
 name_of_hero = ["Азура", "Боэтия", "Вермина", "Джиггалаг", "Клавикус", "М'Айк", "Малакат", "Меридия", "Мерунес Дагон", "Мефала", "Молаг Бал", "Намира", "Ноктюрнал", "Периайт", "Сангвин", "Ситис", "Уриэль Септим", "Хермеус Мора", "Хирсин", "Шеогорат"]
 bonus_things = sorted([Thing() for _ in range(20)], key=lambda thing: thing.bonus_protect) #сортировка вещей по защите
 
-print(fight(creation_of_heroes(name_of_hero, bonus_things)))
+creation_of_heroes_and_fight(name_of_hero, bonus_things)
 print(Style.RESET_ALL)
