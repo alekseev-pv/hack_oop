df444bedea510cc624897eee7e3b514d8197a7de
Gulyaev <egulyaev1@yandex.ru>
2021-03-20 11:53:41 +0300

    condition

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+class Person:
+    def __init__(self):
+        self.name = name
+        self.health = health
+        self.attack = attack
+        self.defence = defence
+    
+    def SetThings(self, things):
+        pass
+
+    def SubtractHealth(self, attack):
+        pass
+
+class Paladin(Person):
+    def __init__(self):
+        self.defence = self.defence * 2
+    pass
+
+class Warrior(Person):
+    def __init__(self):
+        self.attack = self.attack * 2
+
 
