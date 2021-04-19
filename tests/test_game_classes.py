ff2f2c81d98ceede3cd2a34ecd8673389e52413d
mehonator <mehonator@yandex.ru>
2021-04-19 13:07:16 +0300

    add test

--- tests/test_game_classes.py
+++ tests/test_game_classes.py
@@ -1,5 +1,5 @@
 import pytest
-from game_classes import Person, Thing, Inventory
+from game_classes import Person, Thing, Inventory, InventoryOverflowError
 
 
 PROTECTION = 0.1
@@ -33,16 +33,20 @@ def inventory() -> Inventory:
 
 
 class TestInventory:
-    def test_add_thing(self, inventory: Inventory, sword: Thing):
-        inventory.add_thing(sword)
+    def test_append(self, inventory: Inventory, sword: Thing):
+        inventory.append(sword)
         assert inventory.things[0] == sword
 
     def test_overflow(self, inventory: Inventory, sword: Thing):
         SIZE_INVENTORY = 5
         for _ in range(SIZE_INVENTORY):
-            inventory.add_thing(sword)
+            inventory.append(sword)
 
-        assert not inventory.add_thing(sword), "инвентарь не переполнился"
+        try:
+            inventory.append(sword)
+            assert False, "инвентарь не переполнился"
+        except InventoryOverflowError:
+            pass
 
 
 class TestPerson:
@@ -51,3 +55,29 @@ class TestPerson:
         person.reduce_hit_points(damage)
         exept_hp = HP - damage * (1 - PROTECTION)
         assert person.current_hit_points == exept_hp
+
+    def test_take_thing(self, person: Person) -> None:
+        sword = Thing(
+            name="Мечь",
+            multiplier_hit_points=0.1,
+            protection=0.1,
+            attack_damage=10,
+        )
+        for _ in range(len(person.inventory)):
+            assert person.inventory.append(
+                sword
+            ), "Преждевременно переполнение инвентаря"
+
+    def test_inventory_overflow(self, person: Person) -> None:
+        sword = Thing(
+            name="Мечь",
+            multiplier_hit_points=0.1,
+            protection=0.1,
+            attack_damage=10,
+        )
+        for _ in range(len(person.inventory)):
+            person.inventory.append(sword)
+
+        assert not person.inventory.append(
+            sword
+        ), "Не произошло переполнения инвентаря"
